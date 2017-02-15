3ef3e3b7b2150c1ef227f0f410e161c24f043d07
thammaratrud <thammarat.rud@gmail.com>
2017-02-15 18:53:36 +0700

    add checkin

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -3,6 +3,7 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
 
     function ($scope, $interval, $filter, $ionicSideMenuDelegate, $ionicPopup, $ionicLoading, $location, $ionicHistory, localStorageService, $timeout, $state, $cordovaGeolocation, $http) {
         $scope.user = localStorageService.get('authorizationData');
+
         $scope.exitApp = function () {
             // console.log(authorizationData);
 
@@ -10,6 +11,8 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
                 title: 'Confirm exit application',
                 template: '<div class="text-center">Are you sure you want to exit?</div>'
             });
+
+
 
             confirmPopup.then(function (res) {
                 if (res) {
@@ -35,7 +38,7 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
         }
         // console.log( $scope.user);
         $scope.checkIn = function () {
-            alert('in function checkin');
+            // alert('in function checkin');
             // $scope.res = null;
 
             var Data2 = {
@@ -53,7 +56,7 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
             // on hold
             $http.get(serviceBase + '/api/checkins/userid/' + $scope.user._id).success(function (response) {
                 // alert("1");
-                alert('res : ' + JSON.stringify(response));
+                // alert('res : ' + JSON.stringify(response));
                 // alert("2");
 
                 if (response.status === '' || response.status === 'Not checkin') {
@@ -85,16 +88,95 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
                                 Long: lng
                                 // dateTimeOut: Data1.dateTimeOut
                             };
-                            // alert(Data1);
 
-                            $http.post(serviceBase + '/api/checkins', Data1).success(function (response) {
-                                // $scope.res = response;
-                                localStorageService.set('checked', response);
+                            function calcCrow(lat1, lon1, lat2, lon2) {
+                                var R = 6371; // km
+                                var dLat = toRad(lat2 - lat1);
+                                var dLon = toRad(lon2 - lon1);
+                                var lat1 = toRad(lat1);
+                                var lat2 = toRad(lat2);
+
+                                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                                    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+                                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                var d = R * c;
+                                return d;
+                            }
+                            function toRad(Value) {
+                                return Value * Math.PI / 180;
+                            }
+                            // alert(calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1));
+                            // if (calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1) <= 0.5) {
+
+                            // alert(calcCrow(13.9512277, 100.6229312, lat, lng).toFixed(1));
+                            if (calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1) <= 0.5) {
+
+
+
+
+                                $http.post(serviceBase + '/api/checkins', Data1).success(function (response) {
+                                    // $scope.res = response;
+                                    localStorageService.set('checked', response);
+                                    $ionicLoading.hide();
+                                }).error(function (err) {
+                                    alert('checkIn Failed : ' + err);
+                                    // alert('checkIn Failed');
+                                })
+                            } else {
+                                $scope.data = {}
                                 $ionicLoading.hide();
-                            }).error(function (err) {
-                                alert(err);
-                                alert('checkIn Failed');
-                            })
+                                var myPopup = $ionicPopup.show({
+                                    template: '<input type="text" ng-model="data.remark">',
+                                    title: 'Your location is too far away',
+                                    subTitle: 'Please tell your reason',
+                                    scope: $scope,
+                                    buttons: [
+
+                                        {
+                                            text: '<b>Save</b>',
+                                            type: 'button-positive',
+                                            onTap: function (e) {
+                                                if (!$scope.data.remark) {
+                                                    //don't allow the user to close unless he enters wifi password
+                                                    e.preventDefault();
+                                                } else {
+                                                    return $scope.data.remark;
+                                                    myPopup.close();
+
+
+                                                }
+                                            }
+                                        }
+                                    ]
+                                });
+
+
+                                myPopup.then(function (res) {
+                                    // alert(JSON.stringify(res));
+                                    var Data1 = {
+                                        email: $scope.user.email,
+                                        dateTimeIn: new Date(),
+                                        user: $scope.user,
+                                        Lat: lat,
+                                        Long: lng,
+                                        remark: {
+                                            in: res
+                                        }
+                                        // dateTimeOut: Data1.dateTimeOut
+                                    };
+                                    // alert(JSON.stringify(Data1));
+
+                                    $http.post(serviceBase + '/api/checkins', Data1).success(function (response) {
+                                        // $scope.res = response;
+                                        localStorageService.set('checked', response);
+                                        $ionicLoading.hide();
+                                    }).error(function (err) {
+                                        alert(err);
+                                        alert('checkIn Failed');
+                                    })
+                                    console.log('Tapped!', res);
+                                });
+                            };
 
                             // alert('end /api/checkins/userid/' + $scope.user._id);
 
@@ -112,17 +194,117 @@ myApp.controller('homeController', ['$scope', '$interval', '$filter', '$ionicSid
                         });
 
                 } else if (response.status === 'checkin only') {
+                    var posOptions = {
+                        timeout: 10000,
+                        enableHighAccuracy: true
+                    };
+                    var lat = "";
+                    var lng = "";
+                    $ionicLoading.show({
+                        noBackdrop: false,
+                        template: '<p><ion-spinner icon="ripple" class="spinner-assertive"></ion-spinner></p>',
+                    });
+
+
+                    $cordovaGeolocation
+                        .getCurrentPosition(posOptions)
+                        .then(function (position) {
+
+                            lat = position.coords.latitude;
+                            lng = position.coords.longitude;
+
+                            function calcCrow(lat1, lon1, lat2, lon2) {
+                                var R = 6371; // km
+                                var dLat = toRad(lat2 - lat1);
+                                var dLon = toRad(lon2 - lon1);
+                                var lat1 = toRad(lat1);
+                                var lat2 = toRad(lat2);
+
+                                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                                    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+                                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                var d = R * c;
+                                return d;
+                            }
+                            function toRad(Value) {
+                                return Value * Math.PI / 180;
+                            }
+                            alert(calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1));
+                            // if (calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1) <= 0.5) {
+
+                            // alert(calcCrow(13.9512277, 100.6229312, lat, lng).toFixed(1));
+                            if (calcCrow(13.9339768, 100.7178051, lat, lng).toFixed(1) <= 0.5) {
+                                var res = localStorageService.get('checked');
+
+                                res.dateTimeOut = new Date();
+
+                                alert('checkin only : ' + JSON.stringify(res));
+
+
+                                $http.put(serviceBase + '/api/checkins/' + res._id, res).success(function (response) {
+                                    $ionicLoading.hide();
+
+                                })
+                            } else {
+                                $scope.data = {}
+                                $ionicLoading.hide();
+                                var myPopup = $ionicPopup.show({
+                                    template: '<input type="text" ng-model="data.remark">',
+                                    title: 'Your location is too far away',
+                                    subTitle: 'Please tell your reason',
+                                    scope: $scope,
+                                    buttons: [
+
+                                        {
+                                            text: '<b>Save</b>',
+                                            type: 'button-positive',
+                                            onTap: function (e) {
+                                                if (!$scope.data.remark) {
+                                                    //don't allow the user to close unless he enters wifi password
+                                                    e.preventDefault();
+                                                } else {
+                                                    return $scope.data.remark;
+                                                    myPopup.close();
+
+
+                                                }
+                                            }
+                                        }
+                                    ]
+                                });
+
+
+                                myPopup.then(function (res) {
+                                    var res2 = localStorageService.get('checked');
+
+                                    res2.dateTimeOut = new Date();
+                                    // res2.remark = res.remark || {};
+                                    res2.remark.out = res;
+
+
+                                    $http.put(serviceBase + '/api/checkins/' + res2._id, res2).success(function (response) {
+                                        $ionicLoading.hide();
+
+                                    })
+                                    console.log('Tapped!', res);
+                                });
+                            }
+
+
+                        }, function (err) {
+                            alert(JSON.stringify(err));
+                            if (error.code == PositionError.PERMISSION_DENIED) {
+                                alert("Permission denied. check setting");
+                            } else if (error.code == PositionError.POSITION_UNAVAILABLE) {
+                                alert("Cannot get position. May be problem with network or can't get a satellite fix.");
+                            } else if (error.code == PositionError.TIMEOUT) {
+                                alert("Geolocation is timed out.");
+                            } else {
+                                alert(error.message);
+                            }
+                        });
                     // $scope.res
-                    var res = localStorageService.get('checked');
 
-                    res.dateTimeOut = new Date();
-                    alert('checkin only : ' + JSON.stringify(res));
-                    alert('_id : ' + JSON.stringify(res._id));
-                    alert('dateTimeOut : ' + JSON.stringify(res.dateTimeOut));
-
-
-                    $http.put(serviceBase + '/api/checkins/' + res._id, res).success(function (response) {
-                    })
 
                 } else if (response.status === 'checkined today') {
                     alert('today checked completed');
